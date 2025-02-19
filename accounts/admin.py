43f55f6ddc9ae50dcf868d0d6030b1979ab669be
Rajlee <mirajially999@gmail.com>
2025-02-19 21:41:17 +0300

    Updated base.html to fix logo issue

---
+++ accounts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Customize the admin site header, title, and index title
+admin.site.site_header = "Supermarket Administration"
+admin.site.site_title = "Supermarket Admin Portal"
+admin.site.index_title = "Welcome to Supermarket Admin"
